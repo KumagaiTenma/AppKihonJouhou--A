430b9a34f98d641b3a1cef5c2763f351377be888
Your Name <tenma.kumagai.sys@gmail.com>
2017-07-11 10:19:40 +0900

    tenma

---
+++ src/AppKihonJouhou.java
@@ -0,0 +1,27 @@
+public class AppKihonJouhou {
+	int point;
+	int total;
+
+	public static void main(String[] args){
+
+	}
+	public int question1(){
+		    return point;
+    }
+	public int question2(){
+		    return point;
+	}
+	public int question3(){
+		    return point;
+	}
+	public int question4(){
+		    return point;
+	}
+	public int question5(){
+		    return point;
+	}
+	public int question6(){
+		    return point;
+	}
+
+}
